638bd5835018aa5f051db9e85beb231edc44dd85
MrMargaryan <hamlet.java@gmail.com>
2020-07-01 14:48:58 +0300

    heroku  error 19

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use('/api/orders', orderRoute)
 app.use('/api/products', productRoute)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../frontend/build'))
+  app.use(express.static('/frontend/build'))
 
   const __dirname = path.resolve()
 
