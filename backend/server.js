206b17464e73e92b40adb2b34976927c4f3e3b7a
MrMargaryan <hamlet.java@gmail.com>
2020-07-01 13:54:59 +0300

    heroku  error 14

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve()
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build / index.html'))
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
   })
 }
 
